62fd07e778db0abab26bce9e2271c69f1c2ab794
seanarch <zhuqingyun@outlook.com>
2023-06-06 21:48:16 -0600

    Use .env, setup user and chat

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import { ChatEngine } from "react-chat-engine";
 import "./App.css";
 
 function App() {
+  const pid = process.env.REACT_APP_P_ID;
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <ChatEngine
+        height="100vh"
+        projectID={pid}
+        userName="Lica"
+        userSecret="Password01!"
+      />
     </div>
   );
 }
